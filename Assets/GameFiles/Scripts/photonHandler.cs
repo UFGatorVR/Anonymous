012d977d8c0cf05de353fa04fe2b754eca48fb5f
Samantha Garcia <sgarcia22@ufl.edu>
2018-10-23 15:25:05 -0400

    Added VR

---
+++ Assets/GameFiles/Scripts/photonHandler.cs
@@ -0,0 +1,46 @@
+﻿using Photon;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class photonHandler : Photon.PunBehaviour {
+
+    [SerializeField] private PlayerSpawner spawnScript;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.transform);
+        SceneManager.sceneLoaded += OnSceneFinishedLoading;
+        var temp = PhotonVoiceNetwork.Client;
+    }
+
+    public void LoadScene()
+    {
+        PhotonNetwork.LoadLevel("MainGame");
+    }
+
+    public void CreateNewRoom(string text)
+    {
+        PhotonNetwork.CreateRoom(text, new RoomOptions { MaxPlayers = 6 }, null);
+    }
+
+    public void JoinOrCreateRoom(string text)
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 6;
+        PhotonNetwork.JoinOrCreateRoom(text, roomOptions, TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        LoadScene();
+        Debug.Log("We are connected to the room.");
+    }
+
+    private void OnSceneFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.name == "MainGame")
+        {
+            spawnScript.SpawnPlayer();
+        }
+    }
+}
