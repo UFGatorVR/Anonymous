7c467b8fd0231c2108d1155f419928c8b4085105
Samantha Garcia <sgarcia22@ufl.edu>
2018-11-02 14:31:20 -0400

    Still trying to fix the slider

---
+++ Assets/ButtonHandlerServer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonHandlerServer : MonoBehaviour {
+
+    private photonHandler pHandler;
+
+    public void ChangeScene()
+    {
+        pHandler = GameObject.FindGameObjectWithTag("PhotonHandler").GetComponent<photonHandler>();
+        pHandler.JoinOrCreateRoom(gameObject.GetComponentInChildren<Text>().text);
+    }
+}
--- Assets/GameFiles/Scripts/ListRooms/ListController.cs
+++ Assets/GameFiles/Scripts/ListRooms/ListController.cs
@@ -2,30 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon;
 
-public class ListController : MonoBehaviour {
+public class ListController : Photon.MonoBehaviour {
 
     public Button roomName;
     public Text currPlayers;
     public Text maxPlayers;
 
-    private List<RoomClass> rooms;
-    public GameObject listPrefab;
-    public GameObject contentPanel;
+    RoomInfo[] rooms;
+    public RectTransform listPrefab;
+    public ScrollRect scrollView;
+    public RectTransform contentPanel;
+
 
     void Start () {
-        rooms = new List<RoomClass>();
-	}
+        rooms = PhotonNetwork.GetRoomList();
+        UpdateList();
+    }
 	
 	public void UpdateList ()
     {
-        foreach (RoomClass r in rooms)
+        foreach (RoomInfo e in rooms)
         {
-            GameObject newRoom = Instantiate(listPrefab) as GameObject;
-            ListItemController controller = newRoom.GetComponent<ListItemController>();
-            controller.roomName.GetComponentInChildren<Text>().text = r.roomName.GetComponentInChildren<Text>().text;
-            controller.currPlayers.text = r.currPlayers.text;
-            controller.maxPlayers.text = r.maxPlayers.text;
+            RectTransform newRoom = Instantiate(listPrefab) as RectTransform;
+            newRoom.GetChild(0).GetComponentInChildren<Text>().text = e.Name;
+            newRoom.GetChild(1).GetComponent<Text>().text  = e.PlayerCount.ToString();
+            newRoom.GetChild(2).GetComponent<Text>().text = e.MaxPlayers.ToString();
+
             newRoom.transform.parent = contentPanel.transform;
             newRoom.transform.localScale = Vector3.one;
         }
